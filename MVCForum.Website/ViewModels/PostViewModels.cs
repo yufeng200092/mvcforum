b73e5bca989e1e77d1686e1e1a87c3d8413c24e3
yf <yufeng200092@qq.com>
2017-05-23 23:39:40 +0800

    私信编辑器和举报中文

--- MVCForum.Website/ViewModels/PostViewModels.cs
+++ MVCForum.Website/ViewModels/PostViewModels.cs
@@ -59,6 +59,7 @@ namespace MVCForum.Website.ViewModels
         public string PostCreatorUsername { get; set; }
         
         [Required]
+        [ForumMvcResourceDisplayName("Report.Reason")]
         public string Reason { get; set; }
     }
 
